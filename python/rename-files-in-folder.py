c4f1527ce2b7a4c3b7e8bb1cebbfb1740e141c0f
dev <dev.erik@tutamail.com>
2023-10-16 21:03:04 +0300

    feat: add rename files script

---
+++ python/rename-files-in-folder.py
@@ -0,0 +1,15 @@
+import os
+import glob
+
+directory = r'/home/dev/Downloads/asobi'  # your path here
+starting_file_number = 1
+new_file_name = input("Insert your file name: ")
+
+for filename in glob.iglob(directory + '/*.*'):
+    base_name, extension = os.path.splitext(filename)
+    new_file_name_with_original_extension = new_file_name + f"-{starting_file_number}" + extension
+    new_filename = os.path.join(directory, new_file_name_with_original_extension)
+    os.rename(filename, new_filename)
+    starting_file_number += 1
+
+print(f"Files renamed to {new_file_name}-1, {new_file_name}-2, etc.")
