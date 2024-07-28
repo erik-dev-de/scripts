22a56303c760f4ac031e3d64fdc1d40f897ff6c4
erik-dev-de <142629962+erik-dev-de@users.noreply.github.com>
2024-07-28 18:11:27 +0300

    Update choco-fresh-install.bat

--- windows/choco-fresh-install.bat
+++ windows/choco-fresh-install.bat
@@ -4,15 +4,11 @@ choco install steam --yes
 choco install nodejs-lts --yes
 choco install vscode --yes
 choco install git --yes
-choco install microsoft-teams-new-bootstrapper --yes
-choco install postman --yes
 choco install qbittorrent --yes
 choco install onlyoffice --yes
 choco install tutanota --yes
-choco install dbeaver --yes
 choco install intellijidea-community --yes
 choco install google-chrome-x64 --yes
 choco install 7zip --yes
 choco install powershell-core --yes
 choco install lightshot --yes
-choco install postgresql --yes
